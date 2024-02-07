1eeaf29b2893ecef4be0b8bb7fbeaf3dfc284003
Steve Fallet <steve.fallet@divtec.ch>
2024-02-07 16:10:54 +0100

    demon du 7.2

--- js/main.js
+++ js/main.js
@@ -2,8 +2,64 @@
  * Démo mon premier JavaScript
  * @author  Steve Fallet
  * @version 2.0
- * @since   2024-02-07 (date de création)
+ * @since   2024-02-07
  */
 
 'use strict'; // Active le mode strict du JavaScript
+
+const premierP = document.querySelector('p');
+const premierUl = document.querySelector('ul');
+const tousLesStrongs = document.querySelectorAll('strong');
+console.log(premierP, premierUl, tousLesStrongs);
+
+premierP.innerText = 'Bonjour le monde !';
+premierUl.style.backgroundColor = 'yellow';
+
+// Parcours le tableau des tousLesStrongs
+for (let i = 0; i < tousLesStrongs.length; i++){
+    tousLesStrongs[i].style.color = 'red';
+    tousLesStrongs[i].innerText = 'TOTO';
+}
+// Même chose avec for...of
+for(let element of tousLesStrongs) {
+    element.style.textDecoration = 'overline';
+}
+
+/* Gestion de la liste des courses */
+const txtArticle = document.querySelector('#article');
+const btAjouter = document.querySelector('button.ajouter');
+const ulListe = document.querySelector('ul.liste');
+console.log(txtArticle, btAjouter, ulListe);
+
+// Fonction Ajouter
+function ajouter () {
+    // Affiche le texte saisi dans le champ
+    // alert(txtArticle.value);
+    // ulListe.innerHTML += '<li>' + txtArticle.value + '</li>';
+    ulListe.innerHTML += `<li> 
+                            ${txtArticle.value} 
+                          </li>`;
+}
+// Au clic du bouton appelle la fonction ajouter
+// btAjouter.onclick = ajouter;
+// btAjouter.addEventListener('click', ajouter);
+
+// Même chose avec un arrow function anonyme
+btAjouter.addEventListener('click', () => {
+    // Si le texte saisi est plus petit que 1 caractère
+    if(txtArticle.value.length < 1) {
+        alert('Entrez un artile !');
+        return; // Sort de la fonction
+    }
+    ulListe.innerHTML += `<li> ${txtArticle.value} </li>`;
+    txtArticle.value = '';
+    txtArticle.focus();
+});
+
+
+
+
+
+
+
 
